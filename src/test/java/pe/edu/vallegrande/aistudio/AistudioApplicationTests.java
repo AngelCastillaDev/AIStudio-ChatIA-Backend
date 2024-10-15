34dee3fa2071a3504de7f5976846d45c7cdffc84
AngelCastilla <127228998+AngelCastillaSandoval@users.noreply.github.com>
2024-10-15 13:10:13 -0500

    Update AistudioApplicationTests.java

--- src/test/java/pe/edu/vallegrande/aistudio/AistudioApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/aistudio/AistudioApplicationTests.java
@@ -2,12 +2,15 @@ package pe.edu.vallegrande.aistudio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.slf4j;
 
+@Slf4j
 @SpringBootTest
 class AistudioApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Default test");
 	}
 
 }
